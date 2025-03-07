58aaf8ec2d79e7ef10117246bd111f479fc61eac
manue1666 <utm23090708@utma.edu.mx>
2025-03-07 13:15:21 -0600

    endpoints

--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getAllUsers, login, registerUsers } from "./Controllers/UsersController";
+import { createChaska, deleteChaska, getAllChaskas, getChaskas, updateChaska } from "./Controllers/ChaskasController";
 
 //servidor
 const app = express();
@@ -18,8 +19,20 @@ export default app;
 //endpoints
 
 //endpoints de usuarios
-
 app.post("/users/regist",registerUsers)
 app.post("/users/login",login)
+//obtener todos los usuarios
+app.get("/users/getAllUsers",getAllUsers)
 
-app.get("users/getAllUsers",getAllUsers)
+//endpoints de chaskas
+
+//crear chaska
+app.post("/chaskas/create",createChaska)
+//obtener chaskas del usuario
+app.post("/userchaskas/get",getChaskas)
+//obtener todas las chaskas
+app.get("/allchaska/get",getAllChaskas)
+//actualizar chaska
+app.put("/chaskas/update", updateChaska)
+//borrar chaska
+app.delete("/chaska/delete",deleteChaska)
